3b95b307fdbfa3742fe65b9ed445a8141598e0e0
zorothecoder00 <awledouamoussou95@gmail.com>
2026-05-20 15:31:40 +0000

    Fix prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,19 +1,12 @@
-import path from "node:path";
-import { defineConfig, env } from "prisma/config";
-import { config } from "dotenv";
-
-// Charge .env.production si NODE_ENV=production, sinon .env.local puis .env
-const envFile =
-  process.env.NODE_ENV === "production" ? ".env.production" : ".env.local";
-config({ path: path.resolve(process.cwd(), envFile) });
-config({ path: path.resolve(process.cwd(), ".env") }); // fallback
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: path.join("prisma", "schema.prisma"),
+  schema: "prisma/schema.prisma",
   migrations: {
-    path: path.join("prisma", "migrations"),
+    path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL!,
   },
 });
